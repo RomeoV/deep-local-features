d0097e5e68d191f8e08a2614390fc6a35842d97f
Romeo Valentin <rvalentin@student.ethz.ch>
2020-11-21 19:35:05 +0100

    Add Autoencoder and training loop

---
+++ autoencoder.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+from pytorch_lightning.core.lightning import LightningModule
+from AE_data_wrangling import AutoencoderDataModule
+
+from torch.nn import functional as F
+import pytorch_lightning
+
+class LitAutoEncoder(LightningModule):
+  def __init__(self):
+    super().__init__()
+    self.encoder = nn.Sequential(
+        nn.Conv2d(in_channels=64, out_channels=10, kernel_size=(64,64)),
+    )
+    self.decoder = nn.Sequential(
+        nn.ConvTranspose2d(in_channels=10, out_channels=64, kernel_size=(64,64)),
+    )
+  
+  def forward(self, x):
+    embedding = self.encoder(x)
+    return nn.ReLU(embedding)
+  
+  def training_step(self, batch, batch_idx):
+    x = batch['layer1_conv1']
+    z = self.encoder(x)
+    x_hat = self.decoder(z)
+    loss = F.mse_loss(x, x_hat)
+    self.log('train_loss', loss)
+    return loss
+
+  def configure_optimizers(self):
+    optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+    return optimizer
+
+if __name__ == "__main__":
+    autoencoder = LitAutoEncoder()
+    trainer = pytorch_lightning.Trainer(gpus=1 if torch.cuda.is_available() else None)
+    dm = AutoencoderDataModule()
+    trainer.fit(autoencoder, dm)
