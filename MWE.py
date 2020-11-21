9154bd1b57bfb54e0f4ed8f1c083e7ec4da1bcbb
Romeo Valentin <rvalentin@student.ethz.ch>
2020-11-21 19:35:19 +0100

    Slightly simplify MWE

--- MWE.py
+++ MWE.py
@@ -8,12 +8,12 @@ dataset = MegaDepthDataset(scene_list_path=f"{base_path}/train_scenes.txt", scen
 dataset.build_dataset()
 
 first_datapoint = dataset[0]
-repeated_image = torch.stack([first_datapoint['image1'] for _ in range(64)], dim=0)  # to fulfill batch_size = 64
+#repeated_image = torch.stack([first_datapoint['image1'] for _ in range(64)], dim=0)  # to fulfill batch_size = 64
 
 if False:  # plot image1
     Image.fromarray(np.sum(d['image1'].numpy().transpose(1,2,0), axis=2)/3).show()
 
-resnet18 = torchvision.models.resnet18(pretrained=True)
+resnet18 = torchvision.models.resnet18(pretrained=True).eval()
 
 activations = {}
 def get_activation(name):
@@ -24,4 +24,4 @@ def get_activation(name):
 for l in [1,2,3,4]:
     resnet18.__dict__['_modules'][f"layer{l}"][0].conv1.register_forward_hook(get_activation(f"layer{l}_conv1"))
 
-logits = resnet18(repeated_image)
+logits = resnet18(first_datapoint['image1'].unsqueeze(0))
