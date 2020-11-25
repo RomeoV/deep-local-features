b4e138caceee8e14105ef73c5b6bfa8630c62875
Phil26AT <phil.lindenberger@gmail.com>
2020-11-25 14:12:58 +0100

    Implemented FeatureEncoder Module

--- lib/attention_model.py
+++ lib/attention_model.py
@@ -7,12 +7,12 @@ from torch.nn import functional as F
 import pytorch_lightning
 
 from lib.loss import SimilarityLoss
-from lib.autoencoder import LitAutoEncoder
+from lib.autoencoder import FeatureEncoder
 
 class AttentionLayer(LightningModule):
-    def __init__(self, auto_encoder):
+    def __init__(self, feature_encoder):
         super().__init__()
-        self.auto_encoder = auto_encoder
+        self.feature_encoder = feature_encoder
         self.loss = SimilarityLoss()
 
     def softmax(self, ux):
@@ -23,18 +23,22 @@ class AttentionLayer(LightningModule):
             return F.softmax(ux, dim=1)[:,1:2]
     
     def forward(self, x):
-        x = nn.Conv2d(in_channels=10, out_channels=2, kernel_size=(1,1)); #bx2xWxH
+        x = nn.Conv2d(in_channels=self.feature_encoder.encoded_channels, \
+            out_channels=2, kernel_size=(1,1)); #bx2xWxH
         x = softmax(x) #bx1xWxH
         return x
     
     def training_step(self, batch, batch_idx):
-        correspondence = batch['correspondence']
+        x1 = batch['image1']
+        x2 = batch["image2"]
 
-        x1 = batch["activations1"]['layer1_conv1']
-        x2 = batch["activations2"]['layer1_conv1']
+        x1_encoded = self.feature_encoder.forward(x1)
+        x2_encoded = self.feature_encoder.forward(x2)
 
-        x1_encoded = self.auto_encoder.forward(x1)
-        x2_encoded = self.auto_encoder.forward(x2)
+        # We need to concatenate the features of {x1, x2}_encoded here
+
+        x1_encoded.requires_grad = False
+        x2_encoded.requires_grad = False
         y1 = self.forward(x1_encoded)
         y2 = self.forward(x2_encoded)
 
@@ -44,13 +48,18 @@ class AttentionLayer(LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        correspondence = batch['correspondence']
 
-        x1 = batch["activations1"]['layer1_conv1']
-        x2 = batch["activations2"]['layer1_conv1']
+        x1 = batch['image1']
+        x2 = batch["image2"]
 
         x1_encoded = self.auto_encoder.forward(x1)
         x2_encoded = self.auto_encoder.forward(x2)
+
+        # We need to concatenate the features of {x1, x2}_encoded here
+        
+        x1_encoded.requires_grad = False
+        x2_encoded.requires_grad = False
+
         y1 = self.forward(x1_encoded)
         y2 = self.forward(x2_encoded)
 
@@ -63,10 +72,3 @@ class AttentionLayer(LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
-
-if __name__ == "__main__":
-    auto_encoder = LitAutoEncoder()
-    attention = AttentionLayer(auto_encoder)
-    trainer = pytorch_lightning.Trainer(gpus=1 if torch.cuda.is_available() else None)
-    dm = AutoencoderDataModule()
-    trainer.fit(autoencoder, dm)
--- lib/autoencoder.py
+++ lib/autoencoder.py
@@ -1,49 +1,112 @@
 import torch
 from torch import nn
 from pytorch_lightning.core.lightning import LightningModule
-
+import torchvision.models
 
 from torch.nn import functional as F
 import pytorch_lightning
 
+from lib.correspondence_datamodule import ResnetCorrespondenceExtractor, ResnetActivationExtractor
 
-from lib.AE_data_wrangling import AutoencoderDataModule
+class FeatureEncoder(LightningModule):
+    def __init__(self):
+        super().__init__()
 
-class LitAutoEncoder(LightningModule):
-  def __init__(self):
-    super().__init__()
-    self.encoder = nn.Sequential(
-        nn.Conv2d(in_channels=64, out_channels=10, kernel_size=(1,1)),
-    )
-    self.decoder = nn.Sequential(
-        nn.ConvTranspose2d(in_channels=10, out_channels=64, kernel_size=(1,1)),
-    )
-  
-  def forward(self, x):
-    embedding = self.encoder(x)
-    return nn.ReLU(embedding)
-  
-  def training_step(self, batch, batch_idx):
-    x = batch['layer1_conv1']
-    z = self.encoder(x)
-    x_hat = self.decoder(z)
-    loss = F.mse_loss(x, x_hat)
-    self.log('train_loss', loss)
-    return loss
+        self.resnet = torchvision.models.resnet50(pretrained=True)
 
-  def validation_step(self, batch, batch_idx):
-    x = batch['layer1_conv1']
-    z = self.encoder(x)
-    x_hat = self.decoder(z)
-    val_loss = F.mse_loss(x, x_hat)
-    self.log('val_loss', val_loss)
+        self.encoded_channels = 16
 
-  def configure_optimizers(self):
-    optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-    return optimizer
+        self.early_input_channels = 128
+        self.middle_input_channels = 256
+        self.deep_input_channels = 512
+
+        self.resnet_extractor = ResnetActivationExtractor(self.resnet)
+        self.resnet_correspondence_extractor = ResnetCorrespondenceExtractor(self.resnet_extractor)
+
+        self.encoder_early = nn.Sequential(
+            nn.AvgPool2d(kernel_size=(2,2), stride = (2,2)),
+            nn.Conv2d(in_channels=self.early_input_channels, out_channels=self.encoded_channels, kernel_size=(1,1)), 
+            nn.ReLU(True),
+        )
+        self.decoder_early = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=self.encoded_channels, out_channels=self.early_input_channels, kernel_size=(1,1)), 
+            nn.Sigmoid(),
+        )
+
+        self.encoder_middle = nn.Sequential(
+            nn.Conv2d(in_channels=self.middle_input_channels, out_channels=self.encoded_channels, kernel_size=(1,1)), 
+            nn.ReLU(True),
+        )
+        self.decoder_middle = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=self.encoded_channels, out_channels=self.middle_input_channels, kernel_size=(1,1)), 
+            nn.Sigmoid(),
+        )
+
+        self.encoder_deep = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+            nn.Conv2d(in_channels=self.deep_input_channels, out_channels=self.encoded_channels, kernel_size=(1,1)), 
+            nn.ReLU(True),
+        )
+        self.decoder_deep = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=self.encoded_channels, out_channels=self.deep_input_channels, kernel_size=(1,1)), 
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        (x_early, x_middle, x_deep) = self.get_resnet_layers(x)
+
+        y_early = self.encoder_early(x_early)
+        y_middle = self.encoder_middle(x_middle)
+        y_deep = self.encoder_deep(x_deep)
+
+        return [y_early, y_middle, y_deep]
+
+    def training_step(self, batch, batch_idx):
+        (x_early, x_middle, x_deep) = get_resnet_layers(batch["image1"])
+
+        z_early = self.encoder_early(x_early)
+        z_middle = self.encoder_middle(x_middle)
+        z_deep = self.encoder_deep(x_deep)
+
+        x_hat_early = self.decoder_early(z_early)
+        x_hat_middle = self.decoder_middle(z_middle)
+        x_hat_deep = self.decoder_deep(z_deep)
+
+        loss = F.mse_loss(x_early, x_hat_early) + F.mse_loss(x_middle, x_hat_middle) + F.mse_loss(x_deep, x_hat_deep)
+
+        self.log('train_loss', loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        (x_early, x_middle, x_deep) = get_resnet_layers(batch["image1"])
+
+        z_early = self.encoder_early(x_early)
+        z_middle = self.encoder_middle(x_middle)
+        z_deep = self.encoder_deep(x_deep)
+
+        x_hat_early = self.decoder_early(z_early)
+        x_hat_middle = self.decoder_middle(z_middle)
+        x_hat_deep = self.decoder_deep(z_deep)
+
+        loss = F.mse_loss(x_early, x_hat_early) + F.mse_loss(x_middle, x_hat_middle) + F.mse_loss(x_deep, x_hat_deep)
+
+        self.log('validation_loss', loss)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    @torch.no_grad()
+    def get_resnet_layers(self,x):
+        activations = self.resnet_extractor(x)
+        # print(activations.keys())
+        return (activations["layer2_conv1"], activations["layer3_conv1"], activations["layer4_conv1"])
 
 if __name__ == "__main__":
-    autoencoder = LitAutoEncoder()
+    autoencoder = FeatureEncoder()
     trainer = pytorch_lightning.Trainer(gpus=1 if torch.cuda.is_available() else None)
-    dm = AutoencoderDataModule()
+    dm = CorrespondenceDataModule()
     trainer.fit(autoencoder, dm)
