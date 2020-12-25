137dce54f243e4b3e47d3cb8e83bfc83dae84876
Mark Frey <freymark.dev@gmail.com>
2020-12-25 11:25:20 +0100

    receptive field

--- lib/autoencoder.py
+++ lib/autoencoder.py
@@ -8,6 +8,7 @@ import pytorch_lightning
 
 from lib.correspondence_datamodule import ResnetCorrespondenceExtractor, ResnetActivationExtractor, CorrespondenceDataModule
 
+
 class FeatureEncoder(LightningModule):
     def __init__(self):
         super().__init__()
@@ -25,40 +26,50 @@ class FeatureEncoder(LightningModule):
         }
 
         self.resnet_extractor = ResnetActivationExtractor(self.resnet)
-        self.resnet_correspondence_extractor = ResnetCorrespondenceExtractor(self.resnet_extractor)
+        self.resnet_correspondence_extractor = ResnetCorrespondenceExtractor(
+            self.resnet_extractor)
 
         self.encoder = {
             'early': nn.Sequential(
-                nn.Conv2d(in_channels=self.input_channels['early'], out_channels=self.encoded_channels, kernel_size=(1,1)), 
+                nn.Conv2d(
+                    in_channels=self.input_channels['early'], out_channels=self.encoded_channels, kernel_size=(1, 1)),
                 nn.ReLU(True),
-                ),
+            ),
             'middle': nn.Sequential(
-                nn.Conv2d(in_channels=self.input_channels['middle'], out_channels=self.encoded_channels, kernel_size=(1,1)), 
+                nn.Conv2d(
+                    in_channels=self.input_channels['middle'], out_channels=self.encoded_channels, kernel_size=(1, 1)),
                 nn.ReLU(True),
-                ),
+            ),
             'deep': nn.Sequential(
-                nn.Conv2d(in_channels=self.input_channels['deep'], out_channels=self.encoded_channels, kernel_size=(1,1)), 
+                nn.Conv2d(
+                    in_channels=self.input_channels['deep'], out_channels=self.encoded_channels, kernel_size=(1, 1)),
                 nn.ReLU(True),
-                ),
+            ),
         }
 
         self.decoder = {
             'early': nn.Sequential(
-                nn.ConvTranspose2d(in_channels=self.encoded_channels, out_channels=self.input_channels['early'], kernel_size=(1,1)), 
-                #nn.Sigmoid(),
-                ),
+                nn.ConvTranspose2d(in_channels=self.encoded_channels,
+                                   out_channels=self.input_channels['early'], kernel_size=(1, 1)),
+                # nn.Sigmoid(),
+            ),
             'middle': nn.Sequential(
-                nn.ConvTranspose2d(in_channels=self.encoded_channels, out_channels=self.input_channels['middle'], kernel_size=(1,1)), 
-                #nn.Sigmoid(),
-                ),
+                nn.ConvTranspose2d(in_channels=self.encoded_channels,
+                                   out_channels=self.input_channels['middle'], kernel_size=(1, 1)),
+                # nn.Sigmoid(),
+            ),
             'deep': nn.Sequential(
-                nn.ConvTranspose2d(in_channels=self.encoded_channels, out_channels=self.input_channels['deep'], kernel_size=(1,1)), 
-                #nn.Sigmoid(),
-                ),
+                nn.ConvTranspose2d(in_channels=self.encoded_channels,
+                                   out_channels=self.input_channels['deep'], kernel_size=(1, 1)),
+                # nn.Sigmoid(),
+            ),
         }
 
-    def forward(self, x):
-        x = self.get_resnet_layers(x)
+    def forward(self, x, for_receptive_field=False):
+        # if for_receptive_field:
+        #     x = self.get_resnet_layers(x)
+        # else:
+        x = self.get_resnet_layers_no_grad(x)
 
         y = {}
         y['early'] = self.encoder['early'](x['early'])
@@ -68,7 +79,7 @@ class FeatureEncoder(LightningModule):
         return y
 
     def training_step(self, batch, batch_idx):
-        x = self.get_resnet_layers(batch["image1"])
+        x = self.get_resnet_layers_no_grad(batch["image1"])
 
         z = {}
         x_hat = {}
@@ -83,7 +94,7 @@ class FeatureEncoder(LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x = self.get_resnet_layers(batch["image1"])
+        x = self.get_resnet_layers_no_grad(batch["image1"])
 
         z = {}
         x_hat = {}
@@ -102,19 +113,32 @@ class FeatureEncoder(LightningModule):
         return optimizer
 
     @torch.no_grad()
-    def get_resnet_layers(self,x):
+    def get_resnet_layers_no_grad(self, x):
+        return self.get_resnet_layers(x)
+
+    def get_resnet_layers(self, x):
         activations = self.resnet_extractor(x)
+        if len(activations['layer4_conv1'].size()) == 3:
+            deep_features = torch.unsqueeze(activations['layer4_conv1'], 0)
+            middle_features = torch.unsqueeze(activations['layer3_conv1'], 0)
+            early_features = torch.unsqueeze(activations['layer2_conv1'], 0)
+        else:
+            deep_features = activations['layer4_conv1']
+            middle_features = activations['layer3_conv1']
+            early_features = activations['layer2_conv1']
         activation_transform = {
-            'early': nn.AvgPool2d(kernel_size=(2,2), stride = (2,2)),
+            'early': nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
             'middle': lambda x: x,
             'deep': nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
         }
-        return {'early': activation_transform['early'](activations["layer2_conv1"]),
-                'middle': activation_transform['middle'](activations["layer3_conv1"]),
-                'deep': activation_transform['deep'](activations["layer4_conv1"])}
+        return {'early': activation_transform['early'](early_features),
+                'middle': activation_transform['middle'](middle_features),
+                'deep': activation_transform['deep'](deep_features)}
+
 
 if __name__ == "__main__":
     autoencoder = FeatureEncoder()
-    trainer = pytorch_lightning.Trainer(gpus=1 if torch.cuda.is_available() else None)
+    trainer = pytorch_lightning.Trainer(
+        gpus=1 if torch.cuda.is_available() else None)
     dm = CorrespondenceDataModule()
     trainer.fit(autoencoder, dm)
