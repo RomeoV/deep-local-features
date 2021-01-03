5bd280f092dd7eeab9917a72cb7d76dfafc52ad9
Phil26AT <phil.lindenberger@gmail.com>
2021-01-03 15:23:00 +0100

    added distinctiveness Loss

--- lib/loss.py
+++ lib/loss.py
@@ -197,95 +197,111 @@ class TripletMarginLoss(nn.Module):
             torch.sum(scores1 * scores2))
 
 
-# class D2DLoss(nn.Module):
-#     def __init__(self, margin=1, safe_radius=4, scaling_steps=3):
-#         super().__init__()
-#         self.margin = margin
-#         self.safe_radius = safe_radius
-#         self.scaling_steps = scaling_steps
-#         self.device = torch.device('cuda' if torch.cuda.is_available() else "cpu") #torch.device('cpu') if not torch.cuda.is_available() else 
 
-#         self.K = 512
+class DistinctivenessLoss(nn.Module):
+    def __init__(self, margin=1, safe_radius=4, scaling_steps=3):
+        super().__init__()
+        self.margin = margin
+        self.safe_radius = safe_radius
+        self.scaling_steps = scaling_steps
+        self.device = torch.device('cuda' if torch.cuda.is_available() else "cpu") #torch.device('cpu') if not torch.cuda.is_available() else 
+        
+        self.tau = 0.25
+        self.K = 512
 
-#         self.plot = False
+        self.plot = False
     
-#     def forward(self, x1_encoded, x2_encoded, attentions1, attentions2, correspondences):
-#         return 0.0
-#     def correspondence_loss(self, x1_encoded, x2_encoded, attentions1, attentions2, correspondences):
-#         self, x1_encoded, x2_encoded, attentions1, \
-#         attentions2, correspondences, idx):
-#         """
-#         x1_encoded: 1xBx48xWxH (dense_features1) (usually x32x32)
-#         x2_encoded: 1xBx48xWxH (dense_features2) (usually x32x32)
-#         attentions1: 1xBx1xWxH (scores1)
-#         attentions2: 1xBx1xWxH (scores2)
-#         correspondences (image1: 1xBx3x256x256 = 1xBx3xw1xh1, ...) -> 32 * 8 = 256 --> 3 scaling steps
-#         idx: batch index (basically loop over all images in the batch)
-#         out: scalar tensor (1)
-#         """
+    def forward(self, x1_encoded, x2_encoded, attentions1, attentions2, correspondences):
+        loss = torch.tensor(np.array([0], dtype=np.float32), device = self.device)
+        #ToDo: only count valid samples!
+        n_valid = 0.0
+        for idx in range(x1_encoded.shape[0]):
+            l = self.distinctiveness_loss(x1_encoded, x2_encoded, attentions1, attentions2, correspondences, idx)
+            if l is not None:
+                loss += l
+                n_valid += 1.0
+        return loss / n_valid
+    
+    def distinctiveness_loss(self, x1_encoded, x2_encoded, attentions1, attentions2, correspondences, idx):
+        """
+        x1_encoded: 1xBx48xWxH (dense_features1) (usually x32x32)
+        x2_encoded: 1xBx48xWxH (dense_features2) (usually x32x32)
+        attentions1: 1xBx1xWxH (scores1)
+        attentions2: 1xBx1xWxH (scores2)
+        correspondences (image1: 1xBx3x256x256 = 1xBx3xw1xh1, ...) -> 32 * 8 = 256 --> 3 scaling steps
+        idx: batch index (basically loop over all images in the batch)
+        out: scalar tensor (1)
+        """
 
         
-#         loss = torch.tensor(np.array([0], dtype=np.float32))
-#         device = attentions1.device
-#         depth1 = correspondences['depth1'][idx].to(device) # [h1, w1]???
-#         intrinsics1 = correspondences['intrinsics1'][idx].to(device)  # [3, 3]
-#         pose1 = correspondences['pose1'][idx].view(4, 4).to(device)  # [4, 4]
-#         bbox1 = correspondences['bbox1'][idx].to(device) # [2]
+        loss = torch.tensor(np.array([0], dtype=np.float32), device=self.device)
+        depth1 = correspondences['depth1'][idx].to(self.device) # [h1, w1]???
+        intrinsics1 = correspondences['intrinsics1'][idx].to(self.device)  # [3, 3]
+        pose1 = correspondences['pose1'][idx].view(4, 4).to(self.device)  # [4, 4]
+        bbox1 = correspondences['bbox1'][idx].to(self.device) # [2]
 
-#         depth2 = correspondences['depth2'][idx].to(device)
-#         intrinsics2 = correspondences['intrinsics2'][idx].to(device)
-#         pose2 = correspondences['pose2'][idx].view(4, 4).to(device)
-#         bbox2 = correspondences['bbox2'][idx].to(device)
+        depth2 = correspondences['depth2'][idx].to(self.device)
+        intrinsics2 = correspondences['intrinsics2'][idx].to(self.device)
+        pose2 = correspondences['pose2'][idx].view(4, 4).to(self.device)
+        bbox2 = correspondences['bbox2'][idx].to(self.device)
         
 
-#         # Network output
-#         dense_features1 = x1_encoded[idx] #48x32x32
-#         c, h1, w1 = dense_features1.size()
-#         scores1 = attentions1[idx].view(-1) #1x1024 (ids format)
+        # Network output
 
-#         dense_features2 = x2_encoded[idx]
-#         _, h2, w2 = dense_features2.size()
-#         scores2 = attentions2[idx].squeeze(0)
+        dense_features1 = x1_encoded[idx] #48x32x32
+        c, h1, w1 = dense_features1.size()
+        scores1 = attentions1[idx].view(-1) #1x1024 (ids format)
 
-#         all_descriptors1 = F.normalize(dense_features1.view(c, -1), dim=0)# 48x1024, row-major
-#         descriptors1 = all_descriptors1
+        dense_features2 = x2_encoded[idx]
+        _, h2, w2 = dense_features2.size()
+        scores2 = attentions2[idx].squeeze(0)
 
-#         all_descriptors2 = F.normalize(dense_features2.view(c, -1), dim=0)# 48x1024
+        all_descriptors1 = F.normalize(dense_features1.view(c, -1), dim=0)# 48x1024, row-major
+        descriptors1 = all_descriptors1
 
-#         # Warp the positions from image 1 to image 2
-#         fmap_pos1 = grid_positions(h1, w1, self.device) #feature positions, 2x(32*32)=2x1024 [y,x]-format -> [[0,0],[0,1], ...[32,32]]
-#         pos1 = upscale_positions(fmap_pos1, scaling_steps=self.scaling_steps) # feature positions in 256x256, [y,x]-format -> [[0,0],[0,11.5], ...[256,256]]
-#         #ids: matching ids in sequence (256*256)
-#         #default pos1 has ids [0, ..., 1024]
-#         # now ids says which of these are valid based on relative transformation between them, e.g.
-#         # [5, 28, 32, ...,1020]
-#         # so a legal correspondence would be pos1[:,5]<-->pos2[:,0]
-#         try:
-#             pos1, pos2, ids = warp(pos1,
-#                 depth1, intrinsics1, pose1, bbox1,
-#                 depth2, intrinsics2, pose2, bbox2)
-#         except EmptyTensorError:
-#             return loss
-#         fmap_pos1 = fmap_pos1[:, ids] #uv-positions on 32x32 grid, but in list format 2xlen(ids)
-#         descriptors1 = descriptors1[:, ids] #again as list 48xlen(ids)
-#         scores1 = scores1[ids] #again as list 1xlen(ids)
+        all_descriptors2 = F.normalize(dense_features2.view(c, -1), dim=0)# 48x1024
 
-#         # Skip the pair if not enough GT correspondences are available
-#         if ids.size(0) < 128:
-#             return loss
+        # Warp the positions from image 1 to image 2
+        fmap_pos1 = grid_positions(h1, w1, self.device) #feature positions, 2x(32*32)=2x1024 [y,x]-format -> [[0,0],[0,1], ...[32,32]]
+        pos1 = upscale_positions(fmap_pos1, scaling_steps=self.scaling_steps) # feature positions in 256x256, [y,x]-format -> [[0,0],[0,11.5], ...[256,256]]
+        #ids: matching ids in sequence (256*256)
+        #default pos1 has ids [0, ..., 1024]
+        # now ids says which of these are valid based on relative transformation between them, e.g.
+        # [5, 28, 32, ...,1020]
+        # so a legal correspondence would be pos1[:,5]<-->pos2[:,0]
+        try:
+            pos1, pos2, ids = warp(pos1,
+                depth1, intrinsics1, pose1, bbox1,
+                depth2, intrinsics2, pose2, bbox2)
+        except EmptyTensorError:
+            return None
+        fmap_pos1 = fmap_pos1[:, ids] #uv-positions on 32x32 grid, but in list format 2xlen(ids)
+        descriptors1 = descriptors1[:, ids] #again as list 48xlen(ids)
+        scores1 = scores1[ids] #again as list 1xlen(ids)
 
-#         # Descriptors at the corresponding positions
-#         fmap_pos2 = torch.round(
-#             downscale_positions(pos2, scaling_steps=self.scaling_steps)
-#         ).long()
-#         descriptors2 = F.normalize(
-#             dense_features2[:, fmap_pos2[0, :], fmap_pos2[1, :]],
-#             dim=0
-#         )
+        # Skip the pair if not enough GT correspondences are available
+        if ids.size(0) < 128:
+            return None
 
-#         all_fmap_pos2 = grid_positions(h2, w2, self.device)
-#         all_fmap_pos1 = grid_positions(h1, w1, self.device)
-
-#         dtrue = (descriptors1.t().unsqueeze(1) @ descriptors2.t().unsqueeze(2)).squeeze()
-
-#         Ln = F.relu(self.margin + diff))
+        # Descriptors at the corresponding positions
+        fmap_pos2 = torch.round(
+            downscale_positions(pos2, scaling_steps=self.scaling_steps)
+        ).long()
+        descriptors2 = F.normalize(
+            dense_features2[:, fmap_pos2[0, :], fmap_pos2[1, :]],
+            dim=0
+        )
+        scores2 = scores2[fmap_pos2[0, :], fmap_pos2[1, :]].view(-1)
+        
+        distances1 = torch.cdist(descriptors1.t(), all_descriptors2.t()) #shape (338x1024)
+        distances2 = torch.cdist(descriptors2.t(), all_descriptors1.t()) #shape (338x1024)
+        
+        mx1 = torch.sum(distances1 < self.margin,1) - 1.0
+        mx2 = torch.sum(distances2 < self.margin,1) - 1.0
+        
+        target1 = 1.0 / (1.0 + mx1)**self.tau
+        target2 = 1.0 / (1.0 + mx2)**self.tau
+        
+        loss = torch.mean(torch.abs(scores1 - target1)) + torch.mean(torch.abs(scores2 - target2))
+        
+        return loss
