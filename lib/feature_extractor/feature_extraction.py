a8bd8750ce9ab392ec32a40c68329231890eabdb
Mark Frey <freymark.dev@gmail.com>
2021-01-04 15:03:29 +0100

    full extraction

--- lib/feature_extractor/feature_extraction.py
+++ lib/feature_extractor/feature_extraction.py
@@ -36,33 +36,52 @@ def KeypointCenters(boxes):
     return (boxes[:, :2] + boxes[:, 2:])*0.5
 
 
-def ExtractFeatures(feature_map, attention_map, rf, stride, padding, thresh=None, sort=False, normalize_coords=True, image_height=None, image_width=None):
+def ExtractFeatures(feature_map_top, attention_map_top, rf, stride, padding, thresh=None, sort=False, normalize_coords=True, image_height=None, image_width=None):
     if normalize_coords:
         assert(image_height is not None and image_width is not None)
-    boxes = ReceptiveBoxes(
-        feature_map.shape[0], feature_map.shape[1], rf, stride, padding)
-    scores = torch.transpose(attention_map, 0, 1)
-    scores = torch.reshape(scores, [-1])
 
-    features = torch.transpose(feature_map, 0, 1)
-    h, w, c = features.shape
-    features = torch.reshape(features, [h*w, c])
-    if thresh is not None:
-        mask = scores >= thresh
-        boxes = boxes[mask, :]
-        scores = scores[mask]
-        features = features[mask, :]
+    def _extract_features(feature_map, attention_map):
+        attention_map = torch.squeeze(attention_map)
+        feature_map = feature_map.permute(1, 2, 0)
+        h, w, c = feature_map.shape
+        boxes = ReceptiveBoxes(h, w, rf, stride, padding)
+        scores = torch.transpose(attention_map, 0, 1)
+        scores = torch.reshape(scores, [-1])
 
-    if sort:
-        result = torch.sort(scores, descending=True)
-        scores = result.values
-        boxes = boxes[result.indices, :]
-        features = features[result.indices, :]
+        features = torch.transpose(feature_map, 0, 1)
+        features = torch.reshape(features, [h*w, c])
+        if thresh is not None:
+            mask = scores >= thresh
+            boxes = boxes[mask, :]
+            scores = scores[mask]
+            features = features[mask, :]
 
-    centers = KeypointCenters(boxes)
+        if sort:
+            result = torch.sort(scores, descending=True)
+            scores = result.values
+            boxes = boxes[result.indices, :]
+            features = features[result.indices, :]
 
-    if normalize_coords:
-        centers[:, 0] /= image_height
-        centers[:, 1] /= image_width
+        centers = KeypointCenters(boxes)
 
-    return centers, features, scores
+        if normalize_coords:
+            centers[:, 0] /= image_height
+            centers[:, 1] /= image_width
+
+        return centers, features, scores
+
+    if len(feature_map_top.shape) == 3:
+        return _extract_features(feature_map_top, attention_map_top)
+    else:
+        n, _, _, _ = feature_map_top.shape
+        centers = []
+        boxes = []
+        scores = []
+
+        for i in range(n):
+            c, b, s = _extract_features(
+                feature_map_top[i, :, :, :], attention_map_top[i, :, :, :])
+            centers.append(c)
+            boxes.append(b)
+            scores.append(s)
+        return torch.stack(centers), torch.stack(boxes), torch.stack(scores)
--- lib/feature_extractor/tests.py
+++ lib/feature_extractor/tests.py
@@ -1,18 +1,22 @@
 import torch
+import matplotlib.pyplot as plt
 
 from lib.feature_extractor import feature_extraction
 
 
 def main():
-    feature_map = torch.normal(0, 1, size=[30, 40, 16])
+    feature_map = torch.normal(0, 1, size=[16, 30, 40])
     attention_scores = torch.normal(0, 1, size=[30, 40])
 
     centers, features, scores = feature_extraction.ExtractFeatures(
-        feature_map, attention_scores, rf=99.0, stride=16, padding=49.0, thresh=0.1, sort=True, image_height=480, image_width=640)
+        feature_map, attention_scores, rf=99.0, stride=16, padding=49.0, thresh=-1e6, sort=True, normalize_coords=False, image_height=480, image_width=640)
 
     print(centers.shape, centers)
     print(features.shape, features)
     print(scores.shape, scores)
+
+    plt.scatter(centers[:, 0], centers[:, 1])
+    plt.show()
 
 
 if __name__ == "__main__":
