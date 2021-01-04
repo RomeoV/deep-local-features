e9acf2b9af65024aa4a97322ed36dc7a5a8a7dad
Romeo Valentin <rvalentin@student.ethz.ch>
2021-01-04 17:00:54 +0100

    Use BOHB instead of ASHA algorithm

--- tune_autoencoder.py
+++ tune_autoencoder.py
@@ -1,8 +1,10 @@
 from ray.tune.integration.pytorch_lightning import TuneReportCallback
 from ray import tune
-from ray.tune.schedulers import ASHAScheduler
 import pytorch_lightning
 from lib.correspondence_datamodule import CorrespondenceDataModule
+import torch
+from ray.tune.suggest.bohb import TuneBOHB
+from ray.tune.schedulers import HyperBandForBOHB
 
 from lib.autoencoder import FeatureEncoder
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -30,7 +32,7 @@ if __name__ == "__main__":
     num_samples = 10
     num_epochs = 10
     config = {
-            "embedded_layers": tune.choice([32, 64, 128]),
+            "encoded_channels": tune.choice([16, 32, 64, 128]),
             "lr": tune.loguniform(1e-4, 1e-2),
     }
 
@@ -39,24 +41,24 @@ if __name__ == "__main__":
         metric_columns=["loss", "training_iteration"]
     )
 
-    scheduler = ASHAScheduler(
-        max_t=num_epochs,
-        grace_period=1,
-        reduction_factor=2
+    bohb_algo = TuneBOHB(max_concurrent=1)
+    scheduler = HyperBandForBOHB(
+      time_attr="training_iteration",
+      max_t=30
     )
 
     analysis = tune.run(
         tune.with_parameters(
-            train_autoencoder_tune,
-            num_epochs=num_epochs,
+            train_autoencoder_tune
         ),
+        resources_per_trial={"cpu": 4, "gpu": 1},
+        config=config,
+        scheduler=scheduler,
+        search_alg=bohb_algo,
+        progress_reporter=reporter,
+        name="tune_autoencoder_bohb",
         metric="loss",
         mode="min",
-        config=config,
-        num_samples=num_samples,
-        scheduler=scheduler,
-        progress_reporter=reporter,
-        name="tune_mnist_asha"
     )
 
     print("Best hyperparameters found were: ", analysis.best_config)
