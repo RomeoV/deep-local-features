d348f1e2005c464b8a3ba09a45b99a9d44bbf4d1
witi42 <>
2021-01-14 20:53:41 +0100

    typo

--- run_bench.sh
+++ run_bench.sh
@@ -1,42 +1,42 @@
-python .extract features --smart_name --attention_ckpt 
+python .extract_features.py --smart_name --attention_ckpt 
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_d2netloss
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_d2netloss
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_d2netloss_backprop
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_d2netloss_backprop
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_d2netloss
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model_d2netloss
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_d2netloss_backprop
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model_d2netloss_backprop
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_loss
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_loss
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN8_l1
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN8_l1
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN32_l1
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN32_l1
 
-python .extract features --smart_name --attention_ckpt multi_attention_model_distinctiveness_loss
+python .extract_features.py --smart_name --attention_ckpt multi_attention_model_distinctiveness_loss
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_sm_lowmargin_SHARED
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_sm_lowmargin_SHARED
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_lowmargin
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_lowmargin
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda01_sm_SHARED
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda01_sm_SHARED
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda01_sm
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda01_sm
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda1
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda1
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN2_lambda05_sm_SHARED
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN2_lambda05_sm_SHARED
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN2_lambda05_sm
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN2_lambda05_sm
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN64_lambda1
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN64_lambda1
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN32_lambda1
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN32_lambda1
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_sm
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_sm
 
-python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_loss
+python .extract_features.py --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_loss
 
 
 
