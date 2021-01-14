62aa4423f54152d21760f2049257a03dbc6a1d7d
witi42 <>
2021-01-14 20:45:09 +0100

    added new run bench

--- run_bench.sh
+++ run_bench.sh
@@ -1,15 +1,42 @@
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer2 --attention_ckpt cfe64_multi_attention_model2_d2netloss
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer2 --attention_ckpt cfe64_multi_attention_model2_d2netloss_backprop
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer2 --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_loss
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer2 --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda1
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer2 --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN32_lambda1
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer2 --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN64_lambda1
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer --attention_ckpt cfe64_multi_attention_model_d2netloss
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer --attention_ckpt cfe64_multi_attention_model_d2netloss_backprop
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer --attention_ckpt cfe64_multi_attention_model_distinctiveness+_loss
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN8_l1
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN32_l1
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model MultiAttentionLayer --attention_ckpt multi_attention_model_distinctiveness_loss
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model AttentionLayer --attention_ckpt attention_model
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model AttentionLayer --attention_ckpt attention_model_delf_distinctiveness_loss
-python ./extract_features.py --smart_name --load_from_folder --nouse_nms --use_cor_model --encoder_ckpt correspondence_encoder_lr1e3 --attention_model AttentionLayer --attention_ckpt attention_model_sum_distinctiveness_loss
+python .extract features --smart_name --attention_ckpt 
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_d2netloss
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_d2netloss_backprop
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_d2netloss
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_d2netloss_backprop
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_loss
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN8_l1
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model_distinctiveness+_lossN32_l1
+
+python .extract features --smart_name --attention_ckpt multi_attention_model_distinctiveness_loss
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_sm_lowmargin_SHARED
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_lowmargin
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda01_sm_SHARED
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda01_sm
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN8_lambda1
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN2_lambda05_sm_SHARED
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN2_lambda05_sm
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN64_lambda1
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN32_lambda1
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_lossN16_lambda01_sm
+
+python .extract features --smart_name --attention_ckpt cfe64_multi_attention_model2_distinctiveness+_loss
+
+
+
